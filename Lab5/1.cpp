e9bc953df4361291574bef824b81236cab367408
Thanyanit Jongjitragan <46578644+Thanyanit-J@users.noreply.github.com>
2022-12-12 00:15:26 +0700

    Delete Lab5 directory

--- Lab5/1.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-	for(int i=25;i<=125;i+=5){
-		printf("%d ",i);
-	}
-}
--- Lab5/3.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-	for(float hour=0;hour<=4;hour+=0.5){
-		printf("Distance for %.1f hours is %.2f\n",hour,hour*60);
-	}
-}
